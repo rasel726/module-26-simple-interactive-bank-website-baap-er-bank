440554afb2684b56893327cca8a212da19330839
rasel726 <md.rasel2726@gmail.com>
2022-08-19 00:15:47 +0600

    26-4 Navigate to another page, Deposit, withdraw, balance grid

--- js/login.js
+++ js/login.js
@@ -14,10 +14,10 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     // DANGER: DO NOT VERIFY email password in the client side
     // step-4: verify email and password and check whether valid user or not
     if(email === 'sontan@baap.com' && password === 'secret'){
-        console.log('Valid User');
+        window.location.href = 'bank.html';
     }
     else{
-        console.log('Invalid User');
+        alert('Tui password vule gechos!! Toke ami tejjoputro ghosona korlam.');
     }
     
 })
